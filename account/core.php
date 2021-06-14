43660cf801e55afa00d37c3e89fe78844fc0f895
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-14 20:39:39 +0430

    Working on core

---
+++ account/core.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = '127.0.0.1';
+$user = 'narbon';
+$passwd = 'narbon';
+$db = 'jobnic';
+
+$connection = mysqli_connect($server, $user, $passwd, $db);
+
+if (isset($_POST['login'])) {
+
+}
+
+if (isset($_POST['create'])) {
+
+}
