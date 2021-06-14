8f51bd49f512dbef639bb0309e28f1d1bdff6159
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-14 21:25:39 +0430

    Add FA

--- user/index.php
+++ user/index.php
@@ -34,6 +34,7 @@ $row = mysqli_fetch_assoc($result);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index</title>
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <style>
