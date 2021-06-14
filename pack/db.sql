2f8d70f60c5ebf5bd74ef97dcda9381b5d4c76a3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-15 01:23:51 +0430

    Update

--- pack/db.sql
+++ pack/db.sql
@@ -3,6 +3,7 @@ CREATE TABLE `people`
 (
     `row`       int(11) NOT NULL AUTO_INCREMENT,
     `id`        text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `bio`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `firstname` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `lastname`  text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `phone`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -13,13 +14,14 @@ CREATE TABLE `people`
     `instagram` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `github`    text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `telegram`  text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `skills`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     PRIMARY KEY (`row`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 2
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
 
-INSERT INTO people (row, id, firstname, lastname, phone, email, password, twitter, linkedin, instagram, github,
-                    telegram)
-VALUES (1, '458749', 'Amirhossein', 'Mohammadi', '9014784362', 'amir@yahoo.com', '123456', 'GNU_Amir',
-        'amirhosseinmohammadi', 'leonardo_l_larson', 'BlackIQ', 'BlackIQ');
+INSERT INTO people (row, id, bio, firstname, lastname, phone, email, password, twitter, linkedin, instagram, github,
+                    telegram, skills)
+VALUES (1, '458749', 'Hello<br>I am Amir', 'Amirhossein', 'Mohammadi', '9014784362', 'amir@yahoo.com', '123456', 'GNU_Amir',
+        'amirhosseinmohammadi', 'leonardo_l_larson', 'BlackIQ', 'BlackIQ', 'python-50 php-70 c-20');
