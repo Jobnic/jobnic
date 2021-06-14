e65d96a690641a3ff98c9e756057ad15dc019a31
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-14 20:41:02 +0430

    Add names for login

--- account/index.php
+++ account/index.php
@@ -132,15 +132,15 @@
                         <hr>
                         <form action="index.php" method="post">
                             <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                <input name="mail" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                                 <label for="floatingInput">Email address</label>
                             </div>
                             <div class="form-floating">
-                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
                                 <label for="floatingPassword">Password</label>
                             </div>
                             <br>
-                            <button class="btn btn-light mbtn" type="submit">Login</button>
+                            <button name="login" class="btn btn-light mbtn" type="submit">Login</button>
                         </form>
                     </div>
                 </div>
