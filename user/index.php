8c2f5d3b3acb9b10d816a29b906bb1bb7fbf56bf
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-14 21:25:24 +0430

    Add profile dialog

--- user/index.php
+++ user/index.php
@@ -1,7 +1,89 @@
 <?php
 session_start();
+
+$stat = $_SESSION['status'];
+
+if ($stat != true) {
+    ?>
+        <script>
+            window.location.replace("../");
+        </script>
+    <?php
+}
+
+$server = '127.0.0.1';
+$user = 'narbon';
+$passwd = 'narbon';
+$db = 'jobnic';
+
+$connection = mysqli_connect($server, $user, $passwd, $db);
+
 $id = $_SESSION['id'];
 
-echo $id;
+$profile = "SELECT * FROM people WHERE id = '$id'";
+$result = mysqli_query($connection, $profile);
+$row = mysqli_fetch_assoc($result);
 
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <style>
+        body {
+            padding: 8%;
+        }
+        .dialog {
+            border: solid 1px #d3d3d3;
+            padding: 5%;
+        }
+    </style>
+</head>
+<body>
+<div class="">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href=".">Job Nic</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="../account">Logout</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="dialog">
+                    <h3>Profile</h3>
+                    <hr>
+                    <p><b>First Name</b> <?php echo $row['firstname']; ?></p>
+                    <p><b>Last Name</b> <?php echo $row['firstname']; ?></p>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
+</body>
+</html>
