012e41fa89195344c5766734c0f684185f16d3ba
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-15 22:47:24 +0430

    Fix links

--- account/index.php
+++ account/index.php
@@ -41,7 +41,7 @@
 <div class="">
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href=".">Job Nic</a>
+            <a class="navbar-brand" href="../">Job Nic</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -50,7 +50,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../">Home</a>
+                        <a class="nav-link active" aria-current="page" href="../jobs">Jobs</a>
                     </li>
                 </ul>
             </div>
