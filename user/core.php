f33c2ad1d7181cb6d76fc22f8671b25c7e97133c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-15 13:27:09 +0430

    Fix update skills

---
+++ user/core.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+$id = $_SESSION['id'];
+
+$server = '127.0.0.1';
+$user = 'narbon';
+$passwd = 'narbon';
+$db = 'jobnic';
+
+$connection = mysqli_connect($server, $user, $passwd, $db);
+
+$select_user = "SELECT * FROM people WHERE id = '$id'";
+$result_user = mysqli_query($connection, $select_user);
+$row_user = mysqli_fetch_assoc($result_user);
+
+if (isset($_POST["updateskill"])) {
+    $skillname = mysqli_escape_string($connection, $_POST["skillname"]);
+    $skillper = mysqli_escape_string($connection, $_POST["skillper"]);
+
+    $skill = $skillname . "-" . $skillper;
+    $both = $row_user['skills'] . " " . $skill;
+
+    $update_skill = "UPDATE people SET skills = '$both' WHERE id = '$id'";
+
+    if (mysqli_query($connection, $update_skill)) {
+        ?>
+            <script>
+                window.alert("Skills updated.");
+                window.location.replace(".");
+            </script>
+        <?php
+    }
+    else {
+        ?>
+            <script>
+                window.alert("<?php echo mysqli_error($connection); ?>");
+                window.location.replace(".");
+            </script>
+        <?php
+    }
+}
