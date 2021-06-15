0b0b6edd8041d41ebedde7616ad2d8e38d315f2f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-15 20:37:09 +0430

    Fix index nav bar

--- index.php
+++ index.php
@@ -38,14 +38,14 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="jobs">Jobs</a>
                     </li>
                 </ul>
                 <div class="navbar-nav">
                     <?php
                         if ($stat == true) {
                             ?>
-                            <a class="nav-link active" href="user">Go To Panel</a>
+                            <a class="nav-link active" href="user">Go To Panel</a> <a class="nav-link active" href="account/logout.php">Logout</a>
                             <?php
                         }
                         else {
