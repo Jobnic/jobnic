d76c453bd5e2d2a106b5c8f2e4c4522bec5666f9
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-15 20:39:36 +0430

    Fix jobs page nav

--- jobs/index.php
+++ jobs/index.php
@@ -87,14 +87,14 @@ $row = mysqli_fetch_assoc($result);
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href=".">Jobs</a>
                     </li>
                 </ul>
                 <div class="navbar-nav">
                     <?php
                     if ($stat == true) {
                         ?>
-                        <a class="nav-link active" href="../user">Go To Panel</a>
+                        <a class="nav-link active" href="../user">Go To Panel</a> <a class="nav-link active" href="../account/logout.php">Logout</a>
                         <?php
                     }
                     else {
