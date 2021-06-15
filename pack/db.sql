2812a7cca071fa8ae7e9278b3b85aea5dbf70d48
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-15 12:13:06 +0430

    Adding a job table

--- pack/db.sql
+++ pack/db.sql
@@ -3,7 +3,7 @@ CREATE TABLE `people`
 (
     `row`       int(11) NOT NULL AUTO_INCREMENT,
     `id`        text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `bio`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `bio`       text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `firstname` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `lastname`  text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `phone`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `people`
     `instagram` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `github`    text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `telegram`  text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `skills`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `skills`    text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     PRIMARY KEY (`row`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 2
@@ -23,5 +23,27 @@ CREATE TABLE `people`
 
 INSERT INTO people (row, id, bio, firstname, lastname, phone, email, password, twitter, linkedin, instagram, github,
                     telegram, skills)
-VALUES (1, '458749', 'Hello<br>I am Amir', 'Amirhossein', 'Mohammadi', '9014784362', 'amir@yahoo.com', '123456', 'GNU_Amir',
+VALUES (1, '458749', 'Hello<br>I am Amir', 'Amirhossein', 'Mohammadi', '9014784362', 'amir@yahoo.com', '123456',
+        'GNU_Amir',
         'amirhosseinmohammadi', 'leonardo_l_larson', 'BlackIQ', 'BlackIQ', 'python-50 php-70 c-20');
+
+DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE `jobs`
+(
+    `row`      int(11) NOT NULL AUTO_INCREMENT,
+    `jobid`    text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `type`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `user`     text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `title`    text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `describe` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `skills`   text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `status`   text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`row`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+INSERT INTO jobs (row, jobid, type, user, title, `describe`, skills, status)
+VALUES (1, '15948', 'programming', '458749', 'Android Programming', 'We need a Kotlin developer', 'php kotlin linux',
+        'true');
