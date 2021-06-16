f8c470122ef4abde7f88afb073b025834ee3fb5b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-17 01:15:46 +0430

    Add pay page

---
+++ account/pay.php
@@ -0,0 +1,144 @@
+<?php
+session_start();
+
+include('core.php');
+include("../pack/config.php");
+
+$stat = $_SESSION['status'];
+
+if ($stat != true) {
+    ?>
+    <script>
+        window.location.replace("../");
+    </script>
+    <?php
+}
+
+$server = $ip;
+$user = 'narbon';
+$passwd = 'narbon';
+$db = 'jobnic';
+
+$connection = mysqli_connect($server, $user, $passwd, $db);
+
+$id = $_SESSION['id'];
+
+$profile = "SELECT * FROM people WHERE id = '$id'";
+$result = mysqli_query($connection, $profile);
+$row = mysqli_fetch_assoc($result);
+
+if (isset($row['status'])) {
+    ?>
+    <script>
+        window.location.replace("../account/pay.php");
+    </script>
+    <?php
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Job Nic - Pay</title>
+    <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <style>
+        body {
+            padding: 8%;
+        }
+
+        .dialog {
+            border: solid 1px #d3d3d3;
+            padding: 5%;
+        }
+
+        .icon {
+            padding: 5px;
+            border-radius: 5px;
+        }
+
+        .link {
+            text-decoration: none;
+            color: gray;
+        }
+
+        .mbtn {
+            border: solid 1px #d3d3d3;
+        }
+
+        .mbtn:hover {
+            border: solid 1px #d3d3d3;
+        }
+
+        .inp {
+            border: solid 1px #d3d3d3;
+        }
+
+        .inp:hover {
+            border: solid 1px #d3d3d3;
+        }
+    </style>
+</head>
+<body>
+<div class="">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../">Job Nic</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../jobs"><i class="fa fa-list"></i> Jobs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../us/about.php"><i class="fa fa-info"></i> About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../us/contact.php"><i class="fa fa-phone"></i> Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <br>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="dialog">
+                    <p><b><i class="fa fa-question text-secondary"></i> Why seeing this page?</b></p>
+                    <hr>
+                    <p>This means your account time's is up!</p>
+                    <br>
+                    <p><b>What can I do?</b></p>
+                    <p>Pay one more month.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="dialog">
+                    <p><b><i class="fa fa-coins text-secondary"></i> How to pay?</b></p>
+                    <hr>
+                    <p>Via this link below.</p>
+                    <p>
+                        <i class="fa fa-money text-white bg-success icon"></i>
+                        <a class="text-success link" href="http://JobNic.ir.page" target="_blank">Pay from here</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
+</body>
+</html>
