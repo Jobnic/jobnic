d71c92ca58ffb4ed0295796cf35c08d4a4c8eb9d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-17 00:02:05 +0430

    Create review part with close feature

---
+++ user/review.php
@@ -0,0 +1,31 @@
+<?php
+
+if (count($job) > 0) {
+    if ($job[0] == false) {
+        echo '<p class="text-danger">Sorry, job didnt found.</p>';
+    }
+    else {
+        ?>
+        <div>
+            <span style="float: right;" class="btn btn-outline-danger btn-sm"><?php echo $job[0]['type']; ?></span>
+            <p><b><?php echo $job[0]['title']; ?></b></p>
+            <br>
+            <p><?php echo $job[0]['describe']; ?></p>
+            <p style="font-size: 14px;"><?php echo $job[0]['datetime']; ?></p>
+            <?php
+            $skills = explode(" ", $job[0]['skills']);
+
+            foreach ($skills as $skill) {
+                echo "<p class='btn btn-outline-secondary btn-sm'>$skill</p>&nbsp;";
+            }
+            ?>
+            <br>
+            <br>
+            <a class="btn btn-danger btn-sm" href="index.php?close=<?php echo $job['0']['jobid']; ?>">Close Job</a>
+        </div>
+        <?php
+    }
+}
+else {
+    echo '<p class="text-secondary">Select job first.</p>';
+}
