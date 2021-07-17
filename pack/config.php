6229969fb59834b3ad8a196347f9c1d419110c14
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-17 10:47:11 +0430

    Add congif file of everything

--- pack/config.php
+++ pack/config.php
@@ -1,7 +1,16 @@
 <?php
 
-$ip = "127.0.0.1";
+// Server address
+$server = "";
 
+// Database config
+$user = "";
+$passwd = "";
+$db = "";
+
+// Mail config
 $mailaddr = "";
-
 $mailpass = "";
+
+// Create connection
+$connection = mysqli_connect($server, $user, $passwd, $db);
