a718b438f201528fa52b85f915eb33acbe50c075
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-18 14:24:23 +0430

    Remove useless line that are in same core and index

--- user/index.php
+++ user/index.php
@@ -4,30 +4,11 @@ session_start();
 include('core.php');
 include("../pack/config.php");
 
-$stat = $_SESSION['status'];
-
-if ($stat != true) {
-    ?>
-    <script>
-        window.location.replace("../");
-    </script>
-    <?php
-}
-
 $id = $_SESSION['id'];
 
 $profile = "SELECT * FROM people WHERE id = '$id'";
 $result = mysqli_query($connection, $profile);
 $row = mysqli_fetch_assoc($result);
-
-if ($row['status'] == 'not') {
-    ?>
-    <script>
-        window.alert("Sorry, Your account should be active.");
-        window.location.replace("../account/pay.php");
-    </script>
-    <?php
-}
 
 ?>
 
