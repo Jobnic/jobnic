f3d0b2b8a318d427a38f9993e3c20b3e716a5d8a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-19 16:32:34 +0430

    Working on get token

--- account/activate.php
+++ account/activate.php
@@ -2,6 +2,15 @@
 
 session_start();
 
+$stat = $_SESSION['status'];
+if ($stat != true) {
+    ?>
+    <script>
+        window.location.replace("../");
+    </script>
+    <?php
+}
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -10,6 +19,101 @@ require '../pack/mailer/vendor/phpmailer/phpmailer/src/PHPMailer.php';
 require '../pack/mailer/vendor/phpmailer/phpmailer/src/SMTP.php';
 
 include("../pack/config.php");
+
+$id = $_SESSION['id'];
+
+$select_user = "SELECT * FROM people WHERE id = '$id'";
+$result_user = mysqli_query($connection, $select_user);
+$row_user = mysqli_fetch_assoc($result_user);
+
+$name = $row_user['firstname'];
+$email = $row_user['email'];
+
+if (isset($_GET['send'])) {
+    $token = $row_user['token'];
+
+    $send = new PHPMailer;
+
+    $send->IsSMTP();
+    $send->SMTPAuth = true;
+    $send->Host = "smtp.zoho.com";
+    $send->Port = 587;
+    $send->Username = $mailaddr;
+    $send->Password = $mailpass;
+    $send->SMTPSecure = 'tsl';
+    $send->Subject = "Active token";
+
+    $send->setFrom($mailaddr, 'Jobnic');
+    $send->addAddress($email);
+    $send->isHTML(true);
+
+    $link = "http://127.0.0.1/jobnic/account/activate.php?token=$token";
+
+    $bodyContent = '<h1>Hi dear ' . $name . ',</h1>';
+    $bodyContent .= '<h3>You requested for activation email.</h3>';
+    $bodyContent .= '<h5>You can click on the link below and activate your account soon.</h5>';
+    $bodyContent .= '<h5><a href=' . $link . '>Activate my account</a></h5>';
+    $bodyContent .= '<br>';
+    $bodyContent .= '<small>Jobnic Team, working under Neotrinost LLC.</small>';
+
+    $send->Body = $bodyContent;
+
+    if (!$send->send()) {
+        array_push($errors, 'Message could not be sent. Mailer Error: ' . $send->ErrorInfo);
+    } else {
+        ?>
+        <script>
+            window.alert("Email sent.");
+            window.location.replace(".");
+        </script>
+        <?php
+    }
+}
+
+if (isset($_GET['token'])) {
+    $activation_code = $_GET['token'];
+
+    $activation_sql = "SELECT * FROM people WHERE token = '$token'";
+    $activation_result = mysqli_query($connection, $activation_sql);
+
+    if (mysqli_num_rows($activation_result) == 1) {
+        $activation_mail = new PHPMailer;
+
+        $activation_mail->IsSMTP();
+        $activation_mail->SMTPAuth = true;
+        $activation_mail->Host = "smtp.zoho.com";
+        $activation_mail->Port = 587;
+        $activation_mail->Username = $mailaddr;
+        $activation_mail->Password = $mailpass;
+        $activation_mail->SMTPSecure = 'tsl';
+        $activation_mail->Subject = "Active token";
+
+        $activation_mail->setFrom($mailaddr, 'Jobnic');
+        $activation_mail->addAddress($email);
+        $activation_mail->isHTML(true);
+
+        $bodyContent = '<h1>Hi dear ' . $name . ',</h1>';
+        $bodyContent .= '<h3>Your account activated successfully.</h3>';
+        $bodyContent .= '<br>';
+        $bodyContent .= '<h4>Enjoy.</h4>';
+        $bodyContent .= '<br>';
+        $bodyContent .= '<small>Jobnic Team, working under Neotrinost LLC.</small>';
+
+        $activation_mail->Body = $bodyContent;
+
+        if (!$activation_mail->send()) {
+            array_push($errors, 'Message could not be sent. Mailer Error: ' . $activation_mail->ErrorInfo);
+        } else {
+            ?>
+            <script>
+                window.alert("Your account activated successfully.");
+                window.location.replace("../user");
+            </script>
+            <?php
+        }
+    }
+}
+
 ?>
 
 <!doctype html>
@@ -43,11 +147,18 @@ include("../pack/config.php");
                 <div class="main">
                     <h1><i class="fa fa-times"></i></h1>
                     <br>
-                    <h4>Your account is not activated.</h4>
-                    <h4>Check your Email and active it via link.</h4>
+                    <h4>Hi dear <?php echo $name; ?></h4>
+                    <br>
+                    <p>
+                        Your account is not activated.
+                        <br>
+                        Check your Email and active it via link.
+                    </p>
                     <br>
                     <p>If want the Email again, click on the link below.</p>
-                    <p><a href="#">Send Email again</a></p>
+                    <p><a href="activate.php?send=true">Send Email again</a></p>
+                    <br>
+                    <small>Email sent to <?php echo $email; ?></small>
                 </div>
             </div>
             <div class="col-md-2"></div>
