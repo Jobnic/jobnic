4957f9bc51d7c62965c1f9be03e81566af1910de
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-21 00:46:22 +0430

    Fix user review config

--- user/user.php
+++ user/user.php
@@ -1,13 +1,6 @@
 <?php
 include("../pack/config.php");
 
-$server = $ip;
-$user = 'narbon';
-$passwd = 'narbon';
-$db = 'jobnic';
-
-$connection = mysqli_connect($server, $user, $passwd, $db);
-
 $userid = $_GET['userid'];
 
 $reportid = $_GET['id'];
