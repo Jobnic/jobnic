a062dc748569a6fed9741dd9d1c899e3437ff689
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-25 12:03:35 +0430

    Create php file and not we will start laravel

---
+++ php/pack/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Server address
+$server = "localhost";
+
+// Database config
+$user = "amir";
+$passwd = "amir";
+$db = "jobnic";
+
+// Mail config
+$mailaddr = "no-replay@jobnic.net";
+$mailpass = "Black.0481244859IQ.";
+
+// Create connection
+$connection = mysqli_connect($server, $user, $passwd, $db);
