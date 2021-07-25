46827bd69ec486321135c0912aef2ddeb932f1ac
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-25 13:14:28 +0430

    Remove gitignore

---
+++ php/pack/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Server address
+$server = "localhost";
+
+// Database config
+$user = "amir";
+$passwd = "amir";
+$db = "jobnic";
+
+// Mail config
+$mailaddr = "no-replay@jobnic.net";
+$mailpass = "md5mirParsaNeotrinostPythonPhpJava";
+
+// Create connection
+$connection = mysqli_connect($server, $user, $passwd, $db);
