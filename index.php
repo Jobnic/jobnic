20418c478fb6d7c7a7d9ccd52e49d95e773c322b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-29 01:55:38 +0430

    Fix logo image

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
         <br>
         <div class="row">
             <div class="col-md-4">
-                <img src="pack/logo.jpg">
+                <img src="pack/etc/logo.jpg">
                 <br>
             </div>
             <div class="col-md-4">
