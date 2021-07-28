b41024cdd9982cf6a7d0fe9b90b8920478061866
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-29 03:36:03 +0430

    Pages are all done

--- user/pages/setting.php
+++ user/pages/setting.php
@@ -3,37 +3,37 @@
 ?>
 <div class="row">
     <div class="col-md-6">
-        <h2 class="text-dark">Account Setting</h2>
-        <p class="text-dark">Yed, here is your account setting. Change phone or email or password</p>
-        <div class="dialog border border-dark">
-            <h3 class="text-dark"><i class="fa fa-cog text-dark"></i> Settings</h3>
-            <hr class="border border-dark">
+        <h2 class="text-night">Account Setting</h2>
+        <p class="text-night">Yed, here is your account setting. Change phone or email or password</p>
+        <div class="dialog border-night">
+            <h3 class="text-night"><i class="fa fa-cog text-night"></i> Settings</h3>
+            <hr class="border-night">
             <form method="post" action="index.php" class="">
                 <!--                        <label for="formFileSm" class="form-label">Update profile picture</label>-->
                 <!--                        <input class="form-control form-control-sm" id="formFileSm" type="file">-->
                 <!--                        <hr>-->
                 <div class="form-group">
-                    <p><i class="fa fa-phone text-dark"></i> Change Phone</p>
+                    <p><i class="fa fa-phone text-night"></i> Change Phone</p>
                     <input name="phone" type="text"
-                           class="border border-dark form-control-sm form-control"
+                           class="inp border-night form-control-sm form-control"
                            placeholder="Phone">
                     <br>
-                    <button class="btn btn-sm btn-dark" name="updatephone">Change Phone</button>
+                    <button class="btn btn-sm btn-night" name="updatephone">Change Phone</button>
                 </div>
                 <br>
                 <div class="form-group">
-                    <p><i class="fa fa-envelope text-dark"></i> Change Email</p>
+                    <p><i class="fa fa-envelope text-night"></i> Change Email</p>
                     <input name="email" type="text"
-                           class="border border-dark form-control-sm form-control"
+                           class="inp border-night form-control-sm form-control"
                            placeholder="Email">
                     <br>
-                    <button class="btn btn-sm btn-dark" name="updatemain">Change Email</button>
+                    <button class="btn btn-sm btn-night" name="updatemain">Change Email</button>
                 </div>
-                <hr class="border border-dark">
+                <hr class="border-night">
                 <div class="form-group">
-                    <p><i class="fa fa-key text-dark"></i> Change Password</p>
+                    <p><i class="fa fa-key text-night"></i> Change Password</p>
                     <input name="password" type="password"
-                           class="border border-dark form-control-sm form-control"
+                           class="inp border-night form-control-sm form-control"
                            placeholder="Current password">
                 </div>
                 <br>
@@ -41,7 +41,7 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <input name="newpassword" type="password"
-                                   class="form-control border border-dark form-control-sm"
+                                   class="form-control inp border-night form-control-sm"
                                    placeholder="New Password">
                         </div>
                         <br>
@@ -49,38 +49,38 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <input name="confirmpassword" type="password"
-                                   class="form-control border border-dark form-control-sm"
+                                   class="form-control inp border-night form-control-sm"
                                    placeholder="Confirm Password">
                         </div>
                     </div>
                 </div>
                 <br>
-                <button name="updatepassword" type="submit" class="btn btn-dark btn-sm">Change Password</button>
+                <button name="updatepassword" type="submit" class="btn btn-night btn-sm">Change Password</button>
             </form>
         </div>
         <br>
     </div>
     <div class="col-md-4">
-        <h2 class="text-danger">Delete Account</h2>
-        <p class="text-danger">You can delete your account from here</p>
-        <div class="dialog border border-danger">
-            <h3 class="text-danger"><i class="fa fa-trash text-danger"></i> Delete</h3>
-            <hr class="border border-danger">
+        <h2 class="text-night">Delete Account</h2>
+        <p class="text-night">You can delete your account from here</p>
+        <div class="dialog border border-night">
+            <h3 class="text-night"><i class="fa fa-trash text-night"></i> Delete</h3>
+            <hr class="border border-night">
             <form method="post" action="index.php" class="">
                 <div class="form-group">
-                    <p><i class="fa fa-key text-danger"></i> Enter your password</p>
+                    <p><i class="fa fa-key text-night"></i> Enter your password</p>
                     <input name="password" type="text"
-                           class="border border-danger form-control-sm form-control"
+                           class="inp border-night form-control-sm form-control"
                            placeholder="Password">
                     <br>
                     <div class="form-check">
-                        <input class="form-check-input" name="iknow" type="checkbox" id="accept">
-                        <label class="form-check-label text-danger" for="accept">
+                        <input class="form-check-input inp" name="iknow" type="checkbox" id="accept">
+                        <label class="form-check-label text-night" for="accept">
                             I know what I am doing
                         </label>
                     </div>
                     <br>
-                    <button class="btn btn-sm btn-danger" name="deleteaccount">Delete my account</button>
+                    <button class="btn btn-sm btn-night" name="deleteaccount">Delete my account</button>
                 </div>
             </form>
         </div>
