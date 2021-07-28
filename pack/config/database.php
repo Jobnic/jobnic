b9298c9d2b515109d9d9959dc7a5b74d3e3970ad
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-29 02:14:41 +0430

    Create a config just for database

---
+++ pack/config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database config
+$server = "localhost";
+$user = "amir";
+$passwd = "amir";
+$db = "jobnic";
+
+// Create connection
+$connection = mysqli_connect($server, $user, $passwd, $db);
