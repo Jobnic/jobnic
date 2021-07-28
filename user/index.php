ed4569a5f4e236838a351b5bd1dac9204568b366
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-29 02:53:42 +0430

    Add class and id'

--- user/index.php
+++ user/index.php
@@ -24,9 +24,11 @@ $row = mysqli_fetch_assoc($result);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link href="../pack/css/main.css" type="text/css" rel="stylesheet">
+    <link href="../pack/css/dark.css" type="text/css" rel="stylesheet">
+    <link href="../pack/css/light.css" type="text/css" rel="stylesheet">
 </head>
-<body>
-<div class="dark">
+<body class="dark" id="all">
+<div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="../">Job Nic</a>
