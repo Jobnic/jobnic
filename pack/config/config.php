dd4aed688e853083fc53947c46621566fde2606e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-29 02:22:15 +0430

    Create config again

---
+++ pack/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Database config
+$server = "localhost";
+$user = "amir";
+$passwd = "amir";
+$db = "jobnic";
+
+// Create connection
+$connection = mysqli_connect($server, $user, $passwd, $db);
+
+// Mail config
+$mailaddr = "no-replay@jobnic.net";
+$mailpass = "md5mirParsaNeotrinostPythonPhpJava";
