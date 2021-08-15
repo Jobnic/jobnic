707a6c32d31bf5b39a6754d6e17a32e443def960
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-15 21:56:57 +0430

    Remove useles linking

--- us/index.php
+++ us/index.php
@@ -67,9 +67,6 @@ if (isset($_POST["newmessage"])) {
     <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="../pack/css/main.css" type="text/css" rel="stylesheet">
-    <link href="../pack/css/dark.css" type="text/css" rel="stylesheet">
-    <link href="../pack/css/light.css" type="text/css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
         body {
