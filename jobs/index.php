3a260866d85feb2a25cff1421f30757c2d0886ea
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-27 16:04:46 +0430

    Fix all of jobs theme

--- jobs/index.php
+++ jobs/index.php
@@ -5,6 +5,14 @@ include("../pack/config/config.php");
 
 $stat = $_SESSION['status'];
 
+$id = $_SESSION['id'];
+
+$profile = "SELECT * FROM people WHERE id = '$id'";
+$result = mysqli_query($connection, $profile);
+$row = mysqli_fetch_assoc($result);
+
+$theme = $row['theme'];
+
 if ($stat != true) {
     ?>
     <script>
@@ -34,38 +42,72 @@ $result_jobs = mysqli_query($connection, $get_jobs);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Job Nic - Jobs</title>
     <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.neotrinost.ir/jobnic/css/main.css" type="text/css" rel="stylesheet">
+    <?php
+    if ($theme == "auto") {
+        ?>
+        <link href="https://cdn.neotrinost.ir/jobnic/css/auto.css" type="text/css" rel="stylesheet">
+        <?php
+    }
+    else {
+        ?>
+        <link href="https://cdn.neotrinost.ir/jobnic/css/dark.css" type="text/css" rel="stylesheet">
+        <link href="https://cdn.neotrinost.ir/jobnic/css/light.css" type="text/css" rel="stylesheet">
+        <?php
+    }
+    ?>
+    <link href="https://cdn.neotrinost.ir/jobnic/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
-        body {
-            font-family: 'Roboto', sans-serif;
-            padding: 8%;
+        .see-all {
+            border: solid 1px fuchsia;
+            color: fuchsia;
         }
-
-        .card-body:hover {
-            background: #f6f6f6;
-        }
-
-        .link {
-            text-decoration: none;
-            color: gray;
-        }
-        .btn-outline-info:hover {
+        .see-all:hover {
+            border: solid 1px fuchsia;
             color: white;
+            background: fuchsia;
         }
-        .mbtn {
-            border: 1px solid purple;
-            color: purple;
+        @media (prefers-color-scheme: dark) {
+            .others {
+                color: white;
+                text-decoration: none;
+                border: solid 1px white;
+            }
+            .others:hover {
+                color: black;
+                background: white;
+                border: solid 1px white;
+                text-decoration: none;
+            }
         }
-        .mbtn:hover {
-            background: purple;
-            border: 1px solid purple;
-            color: white;
+        @media (prefers-color-scheme: light) {
+            .others {
+                color: black;
+                text-decoration: none;
+                border: solid 1px black;
+            }
+            .others:hover {
+                color: white;
+                background: black;
+                border: solid 1px black;
+                text-decoration: none;
+            }
+        }
+        @media (prefers-color-scheme: dark) {
+            .jobtitle {
+                color: aqua;
+                text-decoration: none;
+            }
+        }
+        @media (prefers-color-scheme: light) {
+            .jobtitle {
+                color: green;
+                text-decoration: none;
+            }
         }
     </style>
 </head>
-<body>
+<body class="<?php echo $theme; ?>">
 <div class="">
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container-fluid">
@@ -106,7 +148,7 @@ $result_jobs = mysqli_query($connection, $get_jobs);
         <br>
         <div class="row">
             <p class="x">
-                <a href="index.php" class="btn btn-sm btn-outline-light mbtn">See All</a>
+                <a href="index.php" class="btn btn-sm see-all">See All</a>
                 &nbsp;
                 <a href="index.php?type=programming" class="btn btn-sm btn-outline-success">Programming</a>
                 &nbsp;
@@ -118,7 +160,7 @@ $result_jobs = mysqli_query($connection, $get_jobs);
                 &nbsp;
                 <a href="index.php?type=school" class="btn btn-sm btn-outline-secondary">School</a>
                 &nbsp;
-                <a href="index.php?type=costume" class="btn btn-sm btn-outline-dark">Others</a>
+                <a href="index.php?type=costume" class="btn btn-sm others">Others</a>
             </p>
         </div>
         <br>
@@ -127,11 +169,11 @@ $result_jobs = mysqli_query($connection, $get_jobs);
                 if (mysqli_num_rows($result_jobs) > 0) {
                     while ($job_row = mysqli_fetch_assoc($result_jobs)) {
                         ?>
-                        <div class="col-md-4">
-                            <div class="card-body border border-dark">
-                                <p class="text-dark">
+                        <div class="col-md-3">
+                            <div class="dialog">
+                                <p>
                                     <b>
-                                        <a class="link text-dark" href="job.php?jobid=<?php echo $job_row['jobid']; ?>">
+                                        <a class="jobtitle" href="job.php?jobid=<?php echo $job_row['jobid']; ?>">
                                             <?php
                                             if ($job_row['nes'] == 'true') {
                                                 echo '<i class="fa fa-asterisk text-danger"></i>';
@@ -141,14 +183,19 @@ $result_jobs = mysqli_query($connection, $get_jobs);
                                             ?>
                                         </a>
                                     </b>
-                                    <span style="float: right;" class="btn btn-outline-dark btn-sm"><?php echo $job_row['type']; ?></span>
+                                    <span style="float: right;" class="btn btn-outline-primary btn-sm"><?php echo $job_row['type']; ?></span>
                                 </p>
-                                <hr class="border border-dark">
+                                <hr>
+                                <small>
+                                    Open the job in new tab for more information.
+                                </small>
+                                <br>
+                                <br>
                                 <?php
                                 $skills = explode(" ", $job_row['skills']);
 
                                 foreach ($skills as $skill) {
-                                    echo "<span class='btn btn-outline-dark btn-sm'>$skill</span>&nbsp;";
+                                    echo "<span class='btn btn-outline-danger btn-sm'>$skill</span>&nbsp;";
                                 }
                                 ?>
                             </div>
