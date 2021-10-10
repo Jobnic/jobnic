4be40968fda1d46694e9fc328b3589adbe0b2cf4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-10 15:15:19 +0330

    Add nav

---
+++ pack/panels/header.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand jntext" href="/jobnic">Job Nic</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active jntext" aria-current="page" href="/jobnic/jobs"><i class="fa fa-list"></i> Jobs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active jntext" aria-current="page" href="/jobnic/us"><i class="fa fa-bank"></i> We</a>
+                </li>
+            </ul>
+            <div class="navbar-nav">
+                <?php
+                if ($stat == true) {
+                    ?>
+                    <a class="nav-link active jntext" href="/jobnic/user"><i class="fa fa-dashboard"></i> Go To Panel</a> <a
+                            class="nav-link active jntext" href="/jobnic/account/logout.php"><i class="fa fa-sign-out"></i> Logout</a>
+                    <?php
+                } else {
+                    ?>
+                    <a class="nav-link active jntext" href="/jobnic/account/index.php"><i class="fa fa-plus"></i> Sign Up</a> <a
+                            class="nav-link active jntext" href="/jobnic/account/index.php"><i class="fa fa-sign-in"></i> Sign
+                        In</a>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</nav>
