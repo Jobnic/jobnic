fba3e85d7df38a86a612856e49b98f4b5f0391c5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-10 15:15:33 +0330

    Update some parts

--- index.php
+++ index.php
@@ -43,54 +43,17 @@ $stat = $_SESSION['status'];
             padding: 8%;
         }
     </style>
+
+    <link href="pack/cos/main.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div class="">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href=".">Job Nic</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="jobs"><i class="fa fa-list"></i> Jobs</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="us"><i class="fa fa-bank"></i> We</a>
-                    </li>
-                </ul>
-                <div class="navbar-nav">
-                    <?php
-                    if ($stat == true) {
-                        ?>
-                        <a class="nav-link active" href="user"><i class="fa fa-dashboard"></i> Go To Panel</a> <a
-                                class="nav-link active" href="account/logout.php"><i class="fa fa-sign-out"></i> Logout</a>
-                        <?php
-                    } else {
-                        ?>
-                        <a class="nav-link active" href="account/index.php"><i class="fa fa-plus"></i> Sign Up</a> <a
-                                class="nav-link active" href="account/index.php"><i class="fa fa-sign-in"></i> Sign
-                            In</a>
-                        <?php
-                    }
-                    ?>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include("pack/panels/header.php"); ?>
     <br>
     <div class="container">
         <br>
         <div class="row">
-            <div class="col-md-4">
-                <img src="pack/etc/logo.jpg">
-                <br>
-            </div>
-            <div class="col-md-4">
+            <div class="col-md-4 jntext">
                 <h3>Welcome to</h3>
                 <h1>Job Nic <b>Beta</b>!</h1>
                 <hr>
