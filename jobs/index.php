e7a3160ce36449460b539e5ad90a81cfd60f5dd4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-06 21:35:57 +0330

    Remove filter

--- jobs/index.php
+++ jobs/index.php
@@ -42,7 +42,8 @@ $result_jobs = mysqli_query($connection, $get_jobs);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Job Nic - Jobs</title>
     <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.neotrinost.ir/jobnic/css/main.css" type="text/css" rel="stylesheet">
+    <!-- <link href="https://cdn.neotrinost.ir/jobnic/css/main.css" type="text/css" rel="stylesheet"> -->
+    <link href="../pack/cos/main.css" type="text/css" rel="stylesheet">
     <?php
     if ($theme == "auto") {
         ?>
@@ -56,7 +57,8 @@ $result_jobs = mysqli_query($connection, $get_jobs);
         <?php
     }
     ?>
-    <link href="https://cdn.neotrinost.ir/jobnic/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <!-- <link href="https://cdn.neotrinost.ir/jobnic/css/bootstrap.min.css" type="text/css" rel="stylesheet"> -->
+    <link href="../pack/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <style>
         .see-all {
             border: solid 1px fuchsia;
@@ -109,68 +111,16 @@ $result_jobs = mysqli_query($connection, $get_jobs);
 </head>
 <body class="<?php echo $theme; ?>">
 <div class="">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href=".">Job Nic</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../jobs"><i class="fa fa-list"></i> Jobs</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../us"><i class="fa fa-bank"></i> We</a>
-                    </li>
-                </ul>
-                <div class="navbar-nav">
-                    <?php
-                    if ($stat == true) {
-                        ?>
-                        <a class="nav-link active" href="../user"><i class="fa fa-dashboard"></i> Go To Panel</a> <a class="nav-link active" href="../account/logout.php"><i class="fa fa-sign-out"></i> Logout</a>
-                        <?php
-                    }
-                    else {
-                        ?>
-                        <a class="nav-link active" href="../account/index.php"><i class="fa fa-plus"></i> Sign Up</a> <a class="nav-link active" href="../account/index.php"><i class="fa fa-sign-in"></i> Sign In</a>
-                        <?php
-                    }
-                    ?>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include("../pack/panels/header.php"); ?>
     <br>
-    <div class="container">
-        <br>
-        <div class="row">
-            <p class="x">
-                <a href="index.php" class="btn btn-sm see-all">See All</a>
-                &nbsp;
-                <a href="index.php?type=programming" class="btn btn-sm btn-outline-success">Programming</a>
-                &nbsp;
-                <a href="index.php?type=design" class="btn btn-sm btn-outline-danger">Design</a>
-                &nbsp;
-                <a href="index.php?type=android" class="btn btn-sm btn-outline-primary">Android</a>
-                &nbsp;
-                <a href="index.php?type=backend" class="btn btn-sm btn-outline-info">Back-End</a>
-                &nbsp;
-                <a href="index.php?type=school" class="btn btn-sm btn-outline-secondary">School</a>
-                &nbsp;
-                <a href="index.php?type=costume" class="btn btn-sm others">Others</a>
-            </p>
-        </div>
-        <br>
+    <div class="container-fluid index">
         <div class="row">
             <?php
                 if (mysqli_num_rows($result_jobs) > 0) {
                     while ($job_row = mysqli_fetch_assoc($result_jobs)) {
                         ?>
                         <div class="col-md-3">
-                            <div class="dialog">
+                            <div class="jobdialog jnborder">
                                 <p>
                                     <b>
                                         <a class="jobtitle" href="job.php?jobid=<?php echo $job_row['jobid']; ?>">
@@ -183,21 +133,23 @@ $result_jobs = mysqli_query($connection, $get_jobs);
                                             ?>
                                         </a>
                                     </b>
-                                    <span style="float: right;" class="btn btn-outline-primary btn-sm"><?php echo $job_row['type']; ?></span>
+                                    <span style="float: left;" class="btn btn-outline-primary btn-sm"><?php echo $job_row['type']; ?></span>
                                 </p>
                                 <hr>
                                 <small>
-                                    Open the job in new tab for more information.
+                                    <?php echo $job_row['describe']; ?>
                                 </small>
                                 <br>
                                 <br>
-                                <?php
-                                $skills = explode(" ", $job_row['skills']);
+                                <div class="skills">
+                                    <?php
+                                    $skills = explode(" ", $job_row['skills']);
 
-                                foreach ($skills as $skill) {
-                                    echo "<span class='btn btn-outline-danger btn-sm'>$skill</span>&nbsp;";
-                                }
-                                ?>
+                                    foreach ($skills as $skill) {
+                                        echo "<span class='btn jnout btn-sm'>$skill</span>&nbsp;";
+                                    }
+                                    ?>
+                                </div>
                             </div>
                             <br>
                         </div>
@@ -205,7 +157,7 @@ $result_jobs = mysqli_query($connection, $get_jobs);
                     }
                 }
                 else {
-                   echo "<h3 class='text-secondary'>No jobs yet</h3>";
+                   echo "<h3 class='text-secondary'>آگهی یافت نشد.</h3>";
             }
             ?>
         </div>
