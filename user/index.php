6a7868ff75ccb60ed4de1a9462b4db9e820c3ee0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-06 16:24:44 +0330

    Fix settings and delete

--- user/index.php
+++ user/index.php
@@ -106,7 +106,7 @@ $theme = $row['theme'];
     }
     ?>
     <div class="">
-        <div id="profile" style="display: block;">
+        <div id="profile" style="display: none;">
             <div class="row">
                 <div class="col-md-4">
                     <div class="dialog">
@@ -286,8 +286,169 @@ $theme = $row['theme'];
                 </div>
             </div>
         </div>
-        <div id="settings" style="display: none;">
-            Hi
+        <div id="settings" style="display: block;">
+        <div class="row">
+                <div class="col-md-6">
+                    <div class="dialog">
+                        <div class="head">
+                            <h4><i class="fa fa-cog"></i> تنظیمات کاربر</h4>
+                        </div>
+                        <div class="body">
+                            <form method="post" action="index.php" class="">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div>
+                                            <p><i class="fa fa-user"></i> Change First Name</p>
+                                            <input name="fname" type="text"
+                                                class="inp form-control-sm form-control"
+                                                placeholder="First Name">
+                                            <br>
+                                            <button class="btn btn-sm jbtn" name="updatefirstname">Change First Name</button>
+                                        </div>
+                                        <br>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div>
+                                            <p><i class="fa fa-user"></i> Change Last Name</p>
+                                            <input name="lname" type="text"
+                                                class="inp form-control-sm form-control"
+                                                placeholder="Last Name">
+                                            <br>
+                                            <button class="btn btn-sm jbtn" name="updatelastname">Change Last Name</button>
+                                        </div>
+                                        <br>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <p><i class="fa fa-phone"></i> Change Phone</p>
+                                    <input name="phone" type="text"
+                                        class="inp form-control-sm form-control"
+                                        placeholder="Phone">
+                                    <br>
+                                    <button class="btn btn-sm jbtn" name="updatephone">Change Phone</button>
+                                </div>
+                                <br>
+                                <div class="form-group">
+                                    <p><i class="fa fa-envelope"></i> Change Email</p>
+                                    <input name="email" type="text"
+                                        class="inp form-control-sm form-control"
+                                        placeholder="Email">
+                                    <br>
+                                    <button class="btn btn-sm jbtn" name="updatemain">Change Email</button>
+                                </div>
+                                <hr>
+                                <div class="form-group">
+                                    <p><i class="fa fa-key"></i> Change Password</p>
+                                    <input name="password" type="password"
+                                        class="inp form-control-sm form-control"
+                                        placeholder="Current password">
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input name="newpassword" type="password"
+                                                class="form-control inp form-control-sm"
+                                                placeholder="New Password">
+                                        </div>
+                                        <br>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input name="confirmpassword" type="password"
+                                                class="form-control inp form-control-sm"
+                                                placeholder="Confirm Password">
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <button name="updatepassword" type="submit" class="btn jbtn btn-sm">Change Password</button>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+                <div class="col-md-6">
+                    <div class="dialog">
+                        <div class="head">
+                            <h4><i class="fa fa-shield"></i> 2FA</h4>
+                        </div>
+                        <div class="body">
+                            <form method="post" action="index.php" class="">
+                                <div class="form-group">
+                                    <p><i class="fa fa-key"></i> Enter 2FA Email</p>
+                                    <input name="email" type="text"
+                                        class="inp form-control-sm form-control"
+                                        placeholder="2FA Email">
+                                    <br>
+                                    <button class="btn btn-sm jbtn" name="enable2fa">Enable 2FA for my account</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="dialog">
+                        <div class="head">
+                            <h4><i class="fa fa-moon-o"></i> Theme</h4>
+                        </div>
+                        <div class="body">
+                            <form method="post" action="index.php" class="">
+                                <div class="form-group">
+                                    <p><i class="fa fa-sun"></i> Set your mode</p>
+                                    <div>
+                                        <input type="radio" name="mode" value="dark" id="dark">
+                                        <label for="dark"><i class="fa fa-moon"></i> Dark Mode</label>
+                                        &nbsp;
+                                        <input type="radio" name="mode" value="light" id="light">
+                                        <label for="light"><i class="fa fa-sun"></i> Light Mode</label>
+                                    </div>
+                                    <br>
+                                    <div>
+                                        <input type="radio" name="mode" value="auto" id="auto">
+                                        <label for="auto"><i class="fa fa-sun"></i> Auto Mode</label>
+                                    </div>
+                                    <br>
+                                    <small>* Your current mode is <b><?php echo $row["theme"]; ?></b>.</small>
+                                    <br>
+                                    <br>
+                                    <button class="btn btn-sm jbtn" name="changemode">Set mode</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="dialog delete">
+                        <div class="head delete">
+                            <h4><i class="fa fa-trash"></i> حذف حساب کاربری</h4>
+                        </div>
+                        <div class="body delete">
+                            <form method="post" action="index.php" class="">
+                                <div class="form-group">
+                                    <p><i class="fa fa-key"></i> رمز خود را وارد کنید</p>
+                                    <input name="password" type="text"
+                                        class="inp form-control-sm form-control"
+                                        placeholder="رمز">
+                                    <br>
+                                    <div class="form-check">
+                                        <input class="form-check-input inp" name="iknow" type="checkbox" id="accept">
+                                        <label class="form-check-label" for="accept">
+                                            بله، بنده با حذف حساب کاربری ام موافق هستم.
+                                        </label>
+                                    </div>
+                                    <br>
+                                    <button class="btn btn-sm jndelbtn" name="deleteaccount">حذف حساب</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+            </div>
         </div>
     </div>
 </div>
