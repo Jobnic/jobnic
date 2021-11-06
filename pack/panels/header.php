7107ebc92fa7ba489676e5ab7578b0c5b35cf4d3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-06 12:26:30 +0330

    Fix header

--- pack/panels/header.php
+++ pack/panels/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand jntext" href="<?php echo $path; ?>">Job Nic</a>
+        <a class="navbar-brand jntext" href="<?php echo $path; ?>">جاب نیک</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
@@ -9,24 +9,22 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active jntext" aria-current="page" href="<?php echo $path; ?>/jobs"><i class="fa fa-list"></i> Jobs</a>
+                    <a class="nav-link active jntext" aria-current="page" href="<?php echo $path; ?>/jobs"><i class="fa fa-list"></i> آگهی ها</a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link active jntext" aria-current="page" href="<?php echo $path; ?>/us"><i class="fa fa-bank"></i> We</a>
-                </li>
+                </li> -->
             </ul>
             <div class="navbar-nav">
                 <?php
                 if ($stat == true) {
                     ?>
-                    <a class="nav-link active jntext" href="<?php echo $path; ?>/user"><i class="fa fa-dashboard"></i> Go To Panel</a> <a
-                            class="nav-link active jntext" href="<?php echo $path; ?>/account/logout.php"><i class="fa fa-sign-out"></i> Logout</a>
+                    <a class="nav-link active jntext" href="<?php echo $path; ?>/user"><i class="fa fa-dashboard"></i> ورود به پنل</a> <a
+                            class="nav-link active jntext" href="<?php echo $path; ?>/account/logout.php"><i class="fa fa-sign-out"></i> خروج از حساب کاربری</a>
                     <?php
                 } else {
                     ?>
-                    <a class="nav-link active jntext" href="<?php echo $path; ?>/account/index.php"><i class="fa fa-plus"></i> Sign Up</a> <a
-                            class="nav-link active jntext" href="<?php echo $path; ?>/account/index.php"><i class="fa fa-sign-in"></i> Sign
-                        In</a>
+                    <a class="nav-link active jntext" href="<?php echo $path; ?>/account/index.php"><i class="fa fa-sign-in"></i> ورود یا ثبت نام</a>
                     <?php
                 }
                 ?>
