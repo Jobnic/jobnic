b6a4fad5245426f13df97744ca3299ad2c5b3f98
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-11-06 21:36:12 +0330

    Fix template for job

--- jobs/job.php
+++ jobs/job.php
@@ -43,19 +43,9 @@ include('core.php');
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Job Nic - Job Review</title>
     <script src="https://kit.fontawesome.com/4a679d8ec0.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="../pack/cos/main.css" type="text/css" rel="stylesheet">
+    <link href="../pack/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
-        body {
-            font-family: 'Roboto', sans-serif;
-            padding: 8%;
-        }
-
-        .dialog {
-            padding: 5%;
-        }
-
         .icon {
             padding: 5px;
             border-radius: 5px;
@@ -70,73 +60,51 @@ include('core.php');
 </head>
 <body>
 <div class="">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="../">Job Nic</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="."><i class="fa fa-list"></i> Jobs</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../us"><i class="fa fa-bank"></i> We</a>
-                    </li>
-                </ul>
-                <div class="navbar-nav">
-                    <a class="nav-link active" href="../user"><i class="fa fa-dashboard"></i> Go To Panel</a> <a class="nav-link active" href="../account/logout.php"><i class="fa fa-sign-out"></i> Logout</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include("../pack/panels/header.php"); ?>
     <br>
-    <div class="container">
+    <div class="container-fluid" style="padding: 5%;">
         <div class="row">
             <?php
             if (isset($jobid)) {
                 ?>
                 <div class="col-md-8">
-                    <div class="dialog border border-success">
+                    <div class="jobdialog jnborder">
                         <?php
                         $select_job = "SELECT * FROM jobs WHERE jobid = '$jobid'";
                         $result_job = mysqli_query($connection, $select_job);
                         if (mysqli_num_rows($result_job) != 1) {
                             ?>
                             <script>
-                                window.alert("Job didnt found.");
+                                window.alert("آگهی پیدا نشد.");
                                 window.location.replace("../jobs");
                             </script>
                             <?php
                         }
                         $row_job = mysqli_fetch_assoc($result_job);
                         ?>
-                        <span style="float: right;" class="btn btn-outline-danger btn-sm"><?php echo $row_job['type']; ?></span>
-                        <span style="float: right; color: white;">-</span>
-                        <span style="float: right;" class="btn btn-sm btn-outline-dark"><i
+                        <span style="float: left;" class="btn btn-outline-danger btn-sm"><?php echo $row_job['type']; ?></span>
+                        <span style="float: left; color: white;">-</span>
+                        <span style="float: left;" class="btn btn-sm btn-outline-dark"><i
                                     class="fa fa-eye"></i> <?php echo $row_job["views"]; ?></span>
                         <h3 class="text-success"><?php echo $row_job['title']; ?></h3>
                         <hr class="border border-success">
                         <p><b><?php echo $row_job['describe']; ?></b></p>
                         <p><?php echo $row_job['datetime']; ?></p>
-                        <small>Price <?php echo $row_job['price']; ?></small>
+                        <small>بودجه <?php echo $row_job['price']; ?></small>
                         <br>
                         <br>
                         <?php
                         $skills = explode(" ", $row_job['skills']);
 
                         foreach ($skills as $skill) {
-                            echo "<p class='btn btn-outline-info btn-sm'>$skill</p>&nbsp;";
+                            echo "<p class='btn jnout btn-sm'>$skill</p>&nbsp;";
                         }
                         ?>
                     </div>
                     <br>
                 </div>
                 <div class="col-md-4">
-                    <div class="dialog border border-primary">
+                    <div class="jobdialog jnborder">
                         <?php
                         $view = $row_job['views'] + 1;
                         $update = "UPDATE jobs SET views = '$view' WHERE jobid = '$jobid'";
@@ -149,18 +117,18 @@ include('core.php');
 
                         if (empty($row_job['closed'])) {
                             ?>
-                                <h3 class="text-primary"><i class="fa fa-pencil text-primary"></i> Apply for job</h3>
+                                <h3 class="text-primary"><i class="fa fa-pencil text-primary"></i> درخواست برای کار</h3>
                                 <hr class="border border-primary">
-                                <p class="text-primary">You can apply for this job with clicking on button below</p>
+                                <p class="text-primary">با کلیک کردن روی دکمه زیر، میتوانید یک درخواست برای این آگهی ثبت نمایید.</p>
                                 <a href="job.php?send=true&job=<?php echo $jobid; ?>" class="btn btn-primary">Send my application</a>
                             <?php
                         }
                         else {
                             ?>
-                            <h3 class="text-danger"><i class="fa fa-pencil text-danger"></i> Apply for job</h3>
+                            <h3 class="text-danger"><i class="fa fa-pencil text-danger"></i> درخواست برای کار</h3>
                             <hr class="border border-danger">
-                            <div style="text-align: center;">
-                                <button class="btn btn-danger">Job is closed.</button>
+                            <div style="text-align: cenrightter;">
+                                <button class="btn btn-danger">این آگهی بسته شده است.</button>
                             </div>
                             <?php
                         }
