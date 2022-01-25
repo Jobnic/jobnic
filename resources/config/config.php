a3e5d4d9b59a3623195af2de2d93253bc5cfef1d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-01-25 16:36:36 +0330

    Add new configs

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,20 +1,7 @@
 <?php
 
-// Database config
-$server = "localhost";
-$user = "jobnicne";
-$passwd = ":v:510l6HxNQjA";
-$db = "jobnicne_jobnic";
-
 // Create connection
-$connection = mysqli_connect($server, $user, $passwd, $db);
-
-// Site
-$host = "https://jobnic.net";
+$connection = mysqli_connect("localhost", "amir", "amir", "jobnic");
 
 // Server
-$path = "https://jobnic.net";
-
-// Mail config
-$mailaddr = "no-replay@jobnic.net";
-$mailpass = "md5mirParsaNeotrinostPythonPhpJava";
+$path = "http://" . $_SERVER['HTTP_HOST'];
